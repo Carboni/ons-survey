b65945de4e6e3ed17f295ca179b37a277190cde1
David Carboni <david@carboni.io>
2016-06-28 10:41:45 +0100

    Updated survey access codes.

--- access_codes.py
+++ access_codes.py
@@ -1,6 +1,6 @@
 
 access_codes = {
-    "1": {
+    "axc123": {
         "reference_unit_id": "123",
         "reference_unit_name": "ACME Corporation",
         "survey_id": "023",
@@ -8,7 +8,7 @@ access_codes = {
         "form_type_id": "0231",
         "survey_period": "0616"
     },
-    "2": {
+    "dey789": {
         "reference_unit_id": "124",
         "reference_unit_name": "Methods Digital",
         "survey_id": "023",
@@ -16,7 +16,7 @@ access_codes = {
         "form_type_id": "0231",
         "survey_period": "0616"
     },
-    "3": {
+    "nicpriicerulez": {
         "reference_unit_id": "125",
         "reference_unit_name": "People Thinking",
         "survey_id": "023",
