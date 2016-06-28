5c8614d3ce1a2104b6f82b2ed9ed83293084489a
David Carboni <david@carboni.io>
2016-06-28 09:20:25 +0100

    Updated the ping script to GET.

--- ping.sh
+++ ping.sh
@@ -4,8 +4,8 @@ while [ true ]
 do
     curl -i \
     -H "Content-Type: application/json" \
-    -X POST -d '{"username":"xyz","password":"xyz"}' \
-    http://localhost:5000/profile
+    -X GET -d '{"username":"xyz","password":"xyz"}' \
+    http://localhost:5000/?access_code="test"
     
     sleep 1
 done
